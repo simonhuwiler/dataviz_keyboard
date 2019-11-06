e75a54e36a77615c295e704ec340afb7fdb9ec0a
Simon Huwiler <simon.huwiler@ringier.ch>
2019-11-06 18:04:11 +0100

    tests

--- client/webpack.config.babel.js
+++ client/webpack.config.babel.js
@@ -39,6 +39,20 @@ export default {
             {
               test: /\.svg$/,
               loader: 'svg-inline-loader'
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                {
+                  loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
+                  loader: "css-loader" // translates CSS into CommonJS
+                },
+                {
+                  loader: "sass-loader" // compiles Sass to CSS
+                }
+              ]
             }
           ]
     },
